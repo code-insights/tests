cd2282f7b5d5383f8b003c879d1704c8aa344b66
parthmehrotra <30300999+parthmehrotra@users.noreply.github.com>
2017-07-26 13:20:06 -0400

    Update Test2.java

--- DocumentationProcessorTests/Test2.java
+++ DocumentationProcessorTests/Test2.java
@@ -5,4 +5,14 @@ public class Test2 {
 	 */
 	public void testFunction1() {
 	}
+	
+	/** 
+	 * This is a normal jd comment
+	 * @param a val 1
+	 * @param b val 2
+	 * @return their sum
+	 */
+	public int sum(int a, int b) {
+		return a+b;
+	}
 }
